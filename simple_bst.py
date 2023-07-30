128e8a5167655802e7b2150dc9e5d7445c0087c2
assetsathidev1 <meher@assetsathi.in>
2023-07-30 19:13:13 +0530

    done with bst

--- simple_bst.py
+++ simple_bst.py
@@ -67,12 +67,48 @@ class TreeNode:
             else:
                 return False
     
-    def remove(self, value):
+    def remove(self, value, parent_node=None):
         """
         function to remove an element from a bst
-        to-do: learn and implement
         """
-        pass
+        current_node = self
+        while current_node is not None:
+            if value < current_node.value:
+                parent_node = current_node
+                current_node = current_node.left
+            elif value > current_node.value:
+                parent_node = current_node
+                current_node = current_node.right
+            else:
+                if current_node.left is not None and current_node.right is not None:
+                    # two children case
+                    current_node.value = current_node.right.find_min_value()
+                    current_node.right.remove(current_node.value, current_node)
+                elif parent_node is None:
+                    if current_node.left is not None:
+                        current_node.value = current_node.left.value
+                        current_node.right = current_node.left.right
+                        current_node.left = current_node.left.left
+                    elif current_node.right is not None:
+                        current_node.value = current_node.right.value
+                        current_node.left = current_node.right.left
+                        current_node.right = current_node.right.right
+                    else:
+                        print("edgest case!!")
+                        break
+                elif parent_node.left == current_node:
+                    parent_node.left = current_node.left if current_node.left is not None else current_node.right
+                elif parent_node.right == current_node:
+                    parent_node.right = current_node.left if current_node.left is not None else current_node.right
+                break
+        return self
+
+    def find_min_value(self):
+        current_node = self
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
 
 tree = TreeNode(11)
 tree.insert(5)
@@ -89,8 +125,22 @@ tree.insert(1)
 tree.insert(17)
 tree.insert(16)
 
+
+# print(tree.find_min_value())
 # print(tree.right.left.left)
 # tree.inorder_traversal()
 # tree.preorder_traversal()
 # tree.postorder_traversal()
-print(tree.is_exists(16))
+# print(tree.is_exists(16))
+# print("------ rem ----")
+# tree.remove(1)
+# tree.inorder_traversal()
+
+# tree.remove(5)
+# tree.inorder_traversal()
+
+# tree.remove(11)
+# tree.inorder_traversal()
+
+# tree.remove(99)
+# tree.inorder_traversal()
